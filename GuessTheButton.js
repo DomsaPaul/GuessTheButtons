7bc33c54375bb1b6372e9a6a8cc7ceda4d015a6b
Domsa Paul <104144984+DomsaPaul@users.noreply.github.com>
2022-05-05 15:49:11 +0300

    Update GuessTheButton.js

--- GuessTheButton.js
+++ GuessTheButton.js
@@ -1,28 +1,4 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Guess the button</title>
-  </head>
-    <body>
-    <div class="container">
-      <div class="card">
-        <div class="card-body">
-        <style> h1{text-align: center;}</style>
-        <h1>Guess the button</h1>
-    </div>
-      <div class="mb-3">
-        <label class="form-label">You can enter a number to generate as many buttons as you want!</label>
-        <input type="number" class="insert" id="number">
-        <button type="button" class="btn btn-primary" onclick="Buttons();">Save</button>
-      </div>
-        <div class="card">
-      </div>
-    </div>
-  </div>
-  
-    <script>
+  <script>
       function Buttons() {
   var Addnr = document.getElementById("number").value;
   const Winner = Math.floor(Math.random() * (Addnr - 1 + 1)) + 1;
@@ -47,5 +23,3 @@
   }
 }
     </script>
-  </body>
-</html>
